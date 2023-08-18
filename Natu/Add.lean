1314d9e2a79e0eeada80688e3869ab5068b0e9b3
Gabriel Torello <gabriel128@gmail.com>
2023-08-18 20:33:26 +1000

    Add up to ordering

--- Natu/Add.lean
+++ Natu/Add.lean
@@ -12,14 +12,14 @@ instance : Add Natu where
   add := Natu.add
 
 -- Adding the definitions as lemma 
-lemma zero_add (m: Natu) : 0 + m = m := by rfl
+lemma zero_add (m: ℕ') : 0 + m = m := by rfl
 
-lemma add_def (n: Natu) (m: Natu) : succ n + m = succ (n + m) := by rfl
+lemma add_def (n: ℕ') (m: Natu) : succ n + m = succ (n + m) := by rfl
 
 lemma zero_is_0 : zero = 0:= by rfl
 
 -- Lemma 2.2.2
-lemma add_zero (n : Natu) : n + 0 = n := by
+lemma add_zero (n : ℕ') : n + 0 = n := by
   apply math_induction n
   · rfl
   · intro ih
@@ -27,7 +27,7 @@ lemma add_zero (n : Natu) : n + 0 = n := by
   qed
 
 -- Lemma 2.2.3
-lemma add_succ (n m : Natu) : n + succ m = succ (n + m) := by
+lemma add_succ (n m : ℕ') : n + succ m = succ (n + m) := by
   apply math_induction n
   · rfl
   · intro ih
@@ -43,7 +43,7 @@ lemma succ_eq_plus_one (n : Natu) : succ n = n + 1 := by
   qed
 
 -- Proposition 2.2.4
-theorem add_comm (n m : Natu) : n + m = m + n := by
+theorem add_comm (n m : ℕ') : n + m = m + n := by
   apply math_induction n
   · rw [zero_is_0, add_zero, zero_add]
   · intro ih
@@ -51,7 +51,7 @@ theorem add_comm (n m : Natu) : n + m = m + n := by
   qed
 
 -- Proposition 2.2.4
-theorem add_assoc (a b c : Natu) : (a + b) + c = a + (b + c) := by
+theorem add_assoc (a b c : ℕ') : (a + b) + c = a + (b + c) := by
   apply math_induction c
   · rw [zero_is_0, add_zero, add_zero]
   · intro ih
@@ -59,7 +59,7 @@ theorem add_assoc (a b c : Natu) : (a + b) + c = a + (b + c) := by
   qed
 
 -- Proposition 2.2.6
-theorem cancellation_law (a b c : Natu) : a + b = a + c -> b = c := by
+theorem cancellation_law (a b c : ℕ') : a + b = a + c -> b = c := by
   apply math_induction a
   · rw [zero_is_0, zero_add, zero_add]
     intro h
@@ -69,4 +69,41 @@ theorem cancellation_law (a b c : Natu) : a + b = a + c -> b = c := by
     have h2 := succ_elim (a + b) (a + c)
     have h3 := h2 h
     exact ih h3
+  qed
+
+-- Proposition 2.2.8
+theorem positive_closure (a b : ℕ') (h1 : Pos a) : Pos (a + b) := by
+  apply math_induction b
+  · rw [zero_is_0, add_zero]
+    exact h1
+  · intro _
+    rw [add_succ, Pos]
+    by_contra h2
+    apply zero_is_not_succ (a + b)
+    exact h2
+  qed
+
+-- Corollary 2.2.9
+lemma zero_sum_implies_a_zero (a b : ℕ'): a + b = 0 -> a = 0 := by
+  intro (h: a + b = 0)
+  by_contra h1
+  have h2: Pos a := by
+    rw [is_positive]
+    exact h1
+  have h3: Pos (a + b) := positive_closure a b h2
+  exact h3 h
+  qed
+
+lemma zero_sum_implies_b_zero (a b : ℕ'): a + b = 0 -> b = 0 := by
+  intro (h: a + b = 0)
+  rw [add_comm] at h
+  apply zero_sum_implies_a_zero b a
+  assumption
+  qed
+
+theorem zero_sum_implies_zero (a b : ℕ'): a + b = 0 -> a = 0 ∧ b = 0 := by
+  intro h
+  have a' := zero_sum_implies_a_zero _ _ h
+  have b' := zero_sum_implies_b_zero _ _ h
+  exact ⟨a', b'⟩
   qed
--- Natu/Defs.lean
+++ Natu/Defs.lean
@@ -35,6 +35,7 @@ axiom zero_is_not_succ : ∀ m : ℕ', succ m ≠ zero
 -- Axiom 2.4
 axiom succ_elim (m n : ℕ') : succ m = succ n → m = n
 
+def Pred (t : Type u) : Type u := t → Prop
 -- Axiom 2.5
 axiom math_induction (n : ℕ') (P: ℕ' → Prop) (hzero : P zero): (P n → P (succ n)) -> P n
 
@@ -45,3 +46,8 @@ example : 3 ≠ 2 := by
   have h2 : 1 = 0 := succ_elim _ _ h1
   exact zero_is_not_succ zero h2
   qed
+
+-- Definition 2.2.7
+def Pos (n: ℕ'): Prop := n ≠ 0
+
+axiom is_positive: ∀ (n: ℕ'), (Pos n) ↔ n ≠ 0
